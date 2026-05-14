fb62c7903192ec651eca07c6bb1824232694cd34
rpussa01 <rasikabandara94@gmail.com>
2026-05-14 13:37:08 +0800

    Added forced dynamics

--- app/admin/dockets/page.tsx
+++ app/admin/dockets/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 import { endOfDate, perthDateString, startOfDate } from "@/lib/date";
--- app/admin/miners/page.tsx
+++ app/admin/miners/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { revalidatePath } from "next/cache";
 import { prisma } from "@/lib/prisma";
